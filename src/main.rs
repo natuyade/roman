eaa9513a01062837a41d2ba0a2d5613fd1ffc2d6
natuyade <reonname@gmail.com>
2026-01-01 21:57:01 +0900

    add  page(novel_2)  and add enum

--- src/main.rs
+++ src/main.rs
@@ -21,25 +21,60 @@ fn global_style() -> &'static str {
     "
 }
 
-fn get_message(count: i32) -> String {
-    match count {
-        0 => text_data::NOVEL1[0].to_string(),
-        1 => text_data::NOVEL1[1].to_string(),
-        2 => text_data::NOVEL1[2].to_string(),
-        3 => text_data::NOVEL1[3].to_string(),
-        4 => text_data::NOVEL1[4].to_string(),
-        5 => text_data::NOVEL1[5].to_string(),
-        _ => format!("Not found Clicked {} times!", count),
+//Copyの内部処理にCloneが自動で付与されるようになっているが
+//deriveのルール上Copy単体で書いてもCloneは使えない
+//Cloneを覚えていないとCopyが使えない上位魔法だと思えばいい。
+
+//ノベルリスト
+#[derive(Copy, Clone)]
+enum Novel {
+    Novel1,
+    Novel2,
+}
+
+//挿絵リスト
+#[derive(Copy, Clone)]
+enum NovelImg {
+    Novel1,
+    Novel2,
+}
+
+fn get_message(novel: Novel, count: i32) -> String {
+    match novel {
+        Novel::Novel1 => match count {
+            0 => text_data::NOVEL1[0].to_string(),
+            1 => text_data::NOVEL1[1].to_string(),
+            2 => text_data::NOVEL1[2].to_string(),
+            3 => text_data::NOVEL1[3].to_string(),
+            4 => text_data::NOVEL1[4].to_string(),
+            5 => text_data::NOVEL1[5].to_string(),
+            _ => format!("Not found Clicked {} times!", count),
+        },
+
+        Novel::Novel2 => match count {
+            0 => text_data::NOVEL2[0].to_string(),
+            1 => text_data::NOVEL2[1].to_string(),
+            2 => text_data::NOVEL2[2].to_string(),
+            3 => text_data::NOVEL2[3].to_string(),
+            4 => text_data::NOVEL2[4].to_string(),
+            5 => text_data::NOVEL2[5].to_string(),
+            _ => format!("Not found Clicked {} times!", count),
+        },
     }
 }
 
-fn get_image(count: i32) -> &'static str {
-    match count {
-        1 => "/image/ouch.webp",
-        999 => "/image/temmie.webp",
-        9999 => "/image/test.webp",
-        _ => "",
-        //_ => "/image/default.webp",
+fn get_image(img: NovelImg, count: i32) -> &'static str {
+    match img {
+        NovelImg::Novel1 => match count {
+            1 => "/image/ouch.webp",
+            _ => "",
+            //_ => "/image/default.webp",
+        },
+        NovelImg::Novel2 => match count {
+            0 => "/image/temmie.webp",
+            _ => "",
+            //_ => "/image/default.webp",
+        },
     }
 }
 
@@ -95,7 +130,7 @@ fn List() -> impl IntoView {
                     "・"<A href="/novel_1">"『平凡な生活』"</A>
                 </p>
                 <p class="list">
-                    "・"<A href="/novel_2">"novel_2"</A>
+                    "・"<A href="/novel_2">"TESTTTT"</A>
                 </p>
                 <p class="list">
                     "・"<A href="/novel_3">"novel_3"</A>
@@ -159,12 +194,87 @@ fn Novel_1() -> impl IntoView {
         <div>
             <h1>"『平凡な生活』"</h1>
             <img
-                src = move || get_image(count.get())
+                src = move || get_image(NovelImg::Novel1, count.get())
                 style = "
                 width: 650px;
                 "
             />
-            <p style="white-space: pre-line;">{ move || get_message(count.get()) }</p>
+            <p style="white-space: pre-line;">{ move || get_message(Novel::Novel1 , count.get()) }</p>
+
+            // count > 0 のときだけ「前」を表示
+            <Show when=move || {count.get() > 0}>
+                <button class="button left" on:click=minus_click>"前"</button>
+            </Show>
+
+            // count < 5 のときだけ「次」を表示
+            <Show when=move || {count.get() < 5}>
+                <button class="button right" on:click=plus_click>"次"</button>
+            </Show>
+
+        </div>
+    }
+}
+
+#[component]
+fn Novel_2() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    let plus_click = move |_| {
+        let next = count.get() + 1;
+        set_count.set(if next > 5 { 0 } else { next });
+    };
+
+    let minus_click = move |_| {
+        let next = count.get() - 1;
+        set_count.set(if next < 0 { 0 } else { next });
+    };
+
+    view! {
+        <style>
+
+            {r#"
+
+            .button {
+                position: fixed;
+                top: 0;
+                height: 100vh;
+                background: transparent;
+                color: transparent;
+                border: none;
+            }
+            
+            /* hoverで触れている時だけ可視化 */
+            .button:hover {
+                background-color: rgba(0,0,0,0.2);
+                color: rgba(72, 72, 72, 1);
+                transition:
+                    background-color 0.8s,
+                    color 0.8s;
+            }
+
+            .left {
+                left: 0;
+                width: 25vw;
+            }
+
+            .right {
+                right: 0;
+                width: 25vw;
+            }
+
+            "#}
+
+        </style>
+
+        <div>
+            <h1>"『TESTTT』"</h1>
+            <img
+                src = move || get_image(NovelImg::Novel2, count.get())
+                style = "
+                width: 650px;
+                "
+            />
+            <p style="white-space: pre-line;">{ move || get_message(Novel::Novel2 , count.get()) }</p>
 
             // count > 0 のときだけ「前」を表示
             <Show when=move || {count.get() > 0}>
@@ -205,6 +315,7 @@ fn App() -> impl IntoView {
                     <Route path=path!("/") view=HomePage/>
                     <Route path=path!("/list") view=List/>
                     <Route path=path!("/novel_1") view=Novel_1/>
+                    <Route path=path!("/novel_2") view=Novel_2/>
                 </Routes>
             </main>
         </Router>
