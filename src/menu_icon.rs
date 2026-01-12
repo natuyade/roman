8b4cf19ae6cd3f8f3ebb6e16e677f849b3f6a1be
natuyade <natuyade@tuta.io>
2026-01-12 22:06:05 +0900

    add test toggle button for icon

--- src/menu_icon.rs
+++ src/menu_icon.rs
@@ -1,28 +1,58 @@
-use web_sys::CanvasRenderingContext2d;
 use std::f64::consts::PI;
 
-pub fn draw_menu_icon(ctx: &CanvasRenderingContext2d){
+pub fn draw_menu_icon(ctx: &web_sys::CanvasRenderingContext2d){
     
     // 描画処理部分
-    // rectangle生成
+    ctx.set_stroke_style_str("white");
     ctx.set_fill_style_str("white");
+    // 背景生成
     ctx.fill_rect(0.0, 0.0, 320.0, 320.0);
-    ctx.set_fill_style_str("red");
-    ctx.fill_rect(60.0, 30.0, 30.0, 180.0);
-    ctx.set_fill_style_str("blue");
-    ctx.fill_rect(60.0, 30.0, 60.0, 30.0);
-    ctx.set_fill_style_str("blue");
-    ctx.fill_rect(60.0, 130.0, 60.0, 30.0);
+    /*
+     * set_global_composite_operation
+     *  destnation-out=描いた場所を透過
+     *  source-over=通常の描画
+     */
+    ctx.set_global_composite_operation("destination-out").unwrap();
+    // 左上
+    ctx.begin_path();
+    ctx.arc(64.0, 64.0, 64.0, PI, -PI/2.0).unwrap();
+    ctx.line_to(0.0, 0.0);
+    ctx.close_path();
+    ctx.fill();
+    // 右上
+    ctx.begin_path();
+    ctx.arc(256.0, 64.0, 64.0, -PI/2.0, 0.0).unwrap();
+    ctx.line_to(320.0, 0.0);
+    ctx.close_path();
+    ctx.fill();
+    // 左下
+    ctx.begin_path();
+    ctx.arc(64.0, 256.0, 64.0, PI/2.0, PI).unwrap();
+    ctx.line_to(0.0, 320.0);
+    ctx.close_path();
+    ctx.fill();
+    // 右下
+    ctx.begin_path();
+    ctx.arc(256.0, 256.0, 64.0, 0.0, PI/2.0).unwrap();
+    ctx.line_to(320.0, 320.0);
+    ctx.close_path();
+    ctx.fill();
+    // 通常の描画
+    ctx.set_global_composite_operation("source-over").unwrap();
     
-    // パスを使用し線や図形を描く
-    ctx.set_stroke_style_str("yellow");
-    ctx.set_fill_style_str("yellow");
+    // rectangle生成(Pの直線)
+    ctx.set_fill_style_str("black");
+    ctx.fill_rect(50.0, 60.0, 30.0, 140.0);
+    ctx.fill_rect(50.0, 30.0, 60.0, 30.0);
+    ctx.fill_rect(50.0, 130.0, 60.0, 30.0);
+    
+    // パスを使用し線や図形を描く(Pの左下のびよ～ん)
     // パス開始の宣言(ペンを持つ)
     ctx.begin_path();
     // 開始地点に移動(ペンを動かす)
-    ctx.move_to(60.0, 210.0);
+    ctx.move_to(50.0, 200.0);
     // 次の地点を決める(どこまで線を引くか決める)
-    ctx.line_to(90.0, 210.0);
+    ctx.line_to(80.0, 200.0);
     ctx.line_to(10.0, 260.0);
     // 開始地点に戻る
     ctx.close_path();
@@ -33,8 +63,6 @@ pub fn draw_menu_icon(ctx: &CanvasRenderingContext2d){
     ctx.fill();
     
     // 円や弧を描く(今回は1/2ドーナツを描く)
-    ctx.set_stroke_style_str("orange");
-    ctx.set_fill_style_str("orange");
     ctx.begin_path();
     /*
      * ctx.arc(x, y, radius, startAngle, endAngle)
@@ -45,66 +73,130 @@ pub fn draw_menu_icon(ctx: &CanvasRenderingContext2d){
      * =約57.29578°
      */
     // 開始地点(円の中心)を決め半径nのxRadian~yRadianまで線を引く
-    ctx.arc(125.0, 95.0, 65.0, -PI/2.0, PI/2.0).unwrap();
+    ctx.arc(115.0, 95.0, 65.0, -PI/2.0, PI/2.0).unwrap();
     // 今回作る物の関係で隙間が空くので少し左にずらす
-    ctx.line_to(120.0, 160.0);
+    ctx.line_to(110.0, 160.0);
     /* 
      * 弧の描画方向を変えたいときは、
      * arc_with_anticlockwiseを使い第6引数にtrue, falseを追加する
      */
     // 少し円の右を太くしたいので中心をずらす
-    ctx.arc_with_anticlockwise(120.0, 95.0, 35.0, PI/2.0, -PI/2.0, true).unwrap();
+    ctx.arc_with_anticlockwise(110.0, 95.0, 35.0, PI/2.0, -PI/2.0, true).unwrap();
     // 今回作る物の関係で隙間が空くので真上に伸ばす
-    ctx.line_to(120.0, 30.0);
+    ctx.line_to(110.0, 30.0);
     // 開始地点に戻る
     ctx.close_path();
     ctx.fill();
     
     // Pの右上にあるやつ
     ctx.begin_path();
-    ctx.move_to(180.0, 30.0);
+    ctx.arc(145.0, 65.0, 35.0, -PI/2.0, 0.0).unwrap();
+    ctx.line_to(180.0, 30.0);
+    ctx.close_path();
+    ctx.fill();
+    
+    // R上部分
+    ctx.begin_path();
+    ctx.arc(215.0, 95.0, 65.0, -PI/2.0, PI/2.0).unwrap();
+    ctx.line_to(190.0, 150.0);
+    ctx.arc_with_anticlockwise(210.0, 95.0, 35.0, PI/2.0, -PI/2.0, true).unwrap();
+    ctx.line_to(200.0, 60.0);
     ctx.line_to(200.0, 30.0);
-    ctx.line_to(200.0, 50.0);
     ctx.close_path();
     ctx.fill();
     
-    // Rの上部分
+    // R下部分
     ctx.begin_path();
-    ctx.arc(225.0, 95.0, 65.0, -PI/2.0, PI/2.0).unwrap();
-    ctx.line_to(220.0, 160.0);
-    ctx.arc_with_anticlockwise(220.0, 95.0, 35.0, PI/2.0, -PI/2.0, true).unwrap();
-    ctx.line_to(220.0, 30.0);
-    ctx.close_path();
-    ctx.fill();
-    
-    // Rの下部分1
-    ctx.begin_path();
-    ctx.move_to(220.0, 130.0);
-    ctx.line_to(250.0, 150.0);
-    ctx.line_to(200.0, 200.0);
-    ctx.line_to(175.0, 175.0);
-    ctx.close_path();
-    ctx.fill();
-    
-    // Rの下部分2
-    ctx.begin_path();
-    ctx.move_to(220.0, 180.0);
-    ctx.line_to(300.0, 260.0);
+    ctx.move_to(200.0, 140.0);
     ctx.line_to(300.0, 280.0);
-    ctx.line_to(280.0, 280.0);
-    ctx.line_to(200.0, 200.0);
+    ctx.line_to(260.0, 280.0);
+    ctx.line_to(175.0, 165.0);
     ctx.close_path();
     ctx.fill();
     
-    // 2の下の上部分
+    // 2下上部分
     ctx.begin_path();
-    ctx.move_to(150.0, 170.0);
-    ctx.line_to(170.0, 190.0);
-    ctx.line_to(110.0, 250.0);
-    ctx.line_to(70.0, 250.0);
+    ctx.move_to(150.0, 160.0);
+    ctx.line_to(170.0, 180.0);
+    ctx.line_to(100.0, 250.0);
+    ctx.line_to(60.0, 250.0);
     ctx.close_path();
     ctx.fill();
     
-    // 2の下の下部分
-    ctx.fill_rect(70.0, 250.0, 120.0, 30.0);
+    // 2下下部分
+    ctx.fill_rect(60.0, 250.0, 120.0, 30.0);
+}
+
+pub fn draw_menu_icon_false(ctx: &web_sys::CanvasRenderingContext2d){
+    
+    ctx.set_stroke_style_str("green");
+    ctx.set_fill_style_str("green");
+    ctx.fill_rect(0.0, 0.0, 320.0, 320.0);
+    ctx.set_global_composite_operation("destination-out").unwrap();
+    ctx.begin_path();
+    ctx.arc(64.0, 64.0, 64.0, PI, -PI/2.0).unwrap();
+    ctx.line_to(0.0, 0.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.arc(256.0, 64.0, 64.0, -PI/2.0, 0.0).unwrap();
+    ctx.line_to(320.0, 0.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.arc(64.0, 256.0, 64.0, PI/2.0, PI).unwrap();
+    ctx.line_to(0.0, 320.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.arc(256.0, 256.0, 64.0, 0.0, PI/2.0).unwrap();
+    ctx.line_to(320.0, 320.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.set_global_composite_operation("source-over").unwrap();
+    ctx.set_fill_style_str("white");
+    ctx.fill_rect(50.0, 60.0, 30.0, 140.0);
+    ctx.fill_rect(50.0, 30.0, 60.0, 30.0);
+    ctx.fill_rect(50.0, 130.0, 60.0, 30.0);
+    ctx.begin_path();
+    ctx.move_to(50.0, 200.0);
+    ctx.line_to(80.0, 200.0);
+    ctx.line_to(10.0, 260.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.arc(115.0, 95.0, 65.0, -PI/2.0, PI/2.0).unwrap();
+    ctx.line_to(110.0, 160.0);
+    ctx.arc_with_anticlockwise(110.0, 95.0, 35.0, PI/2.0, -PI/2.0, true).unwrap();
+    ctx.line_to(110.0, 30.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.arc(145.0, 65.0, 35.0, -PI/2.0, 0.0).unwrap();
+    ctx.line_to(180.0, 30.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.arc(215.0, 95.0, 65.0, -PI/2.0, PI/2.0).unwrap();
+    ctx.line_to(190.0, 150.0);
+    ctx.arc_with_anticlockwise(210.0, 95.0, 35.0, PI/2.0, -PI/2.0, true).unwrap();
+    ctx.line_to(200.0, 60.0);
+    ctx.line_to(200.0, 30.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.move_to(200.0, 140.0);
+    ctx.line_to(300.0, 280.0);
+    ctx.line_to(260.0, 280.0);
+    ctx.line_to(175.0, 165.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.begin_path();
+    ctx.move_to(150.0, 160.0);
+    ctx.line_to(170.0, 180.0);
+    ctx.line_to(100.0, 250.0);
+    ctx.line_to(60.0, 250.0);
+    ctx.close_path();
+    ctx.fill();
+    ctx.fill_rect(60.0, 250.0, 120.0, 30.0);
 }
--- src/novels/test_1.rs
+++ src/novels/test_1.rs
@@ -1,20 +1,21 @@
 use leptos::prelude::*;
 use leptos::html::Canvas;
-use wasm_bindgen::JsCast;
-use web_sys::CanvasRenderingContext2d;
 
-use crate::menu_icon::draw_menu_icon;
+use crate::menu_icon::{draw_menu_icon, draw_menu_icon_false};
 
 #[component]
 pub fn test_1() -> impl IntoView {
     let canvas_ref: NodeRef<Canvas> = NodeRef::new();
-
+    
+    let (checked, set_checked) = signal(true);
+    
     // EffectはDOMがレンダされた時に発動
     Effect::new(move |_| {
         if let Some(canvas) = canvas_ref.get() {
             let html_canvas: web_sys::HtmlCanvasElement = canvas.clone();
 
             // 2Dコンテキストを取得
+            use wasm_bindgen::JsCast;
             let ctx = html_canvas
                 .get_context("2d")
                 /*
@@ -27,11 +28,17 @@ pub fn test_1() -> impl IntoView {
                  */
                 .unwrap()
                 .unwrap()
-                .dyn_into::<CanvasRenderingContext2d>()
+                .dyn_into::<web_sys::CanvasRenderingContext2d>()
                 .unwrap();
 
-                // 描画処理
-                draw_menu_icon(&ctx);
+                // 描画リフレッシュ
+                ctx.clear_rect(0.0, 0.0, 320.0, 320.0);
+                
+                if checked.get() {
+                    draw_menu_icon(&ctx);
+                } else {
+                    draw_menu_icon_false(&ctx);
+                }
         }
     });
     view! {
@@ -47,19 +54,31 @@ pub fn test_1() -> impl IntoView {
                 .menu {
                     display: block;
                 }
+                
+                .menu-icon {
+                    display: block;
+                    width: 32px;
+                    height: 32px;
+                    background-image: `draw_menu_icon(&ctx)`;
+                    background-size: cover;
+                }
 
                 #toggle:checked ~ .menu {
                     display: none;
                 }
+                
+                #toggle:checked ~ canvas {
+                    rotate: 90deg; 
+                }
             "
         </style>
         <div class="menu-button">
-            <input type="checkbox" id="toggle"/>
-                <label for="toggle"></label>
+                <canvas node_ref=canvas_ref width="320" height="320" on:click=move |_| {
+                    set_checked.update(|c| *c = !*c)
+                    }></canvas>
             <div class="menu">
                 <img src="assets/images/p2r_logo_wh.webp"></img>
             </div>
-            <canvas node_ref=canvas_ref width="320" height="320"></canvas>
         </div>
     }
 }
