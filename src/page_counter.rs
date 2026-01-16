8a7e8e037c0975133043513314ccd49af877a77f
natuyade <natuyade@tuta.io>
2026-01-16 23:46:01 +0900

    add illusts

--- src/page_counter.rs
+++ src/page_counter.rs
@@ -1,7 +1,5 @@
 use crate::text_data;
 
-
-
 //Copyの内部処理にCloneが自動で付与されるようになっているが
 //deriveのルール上Copy単体で書いてもCloneは使えない
 //Cloneを覚えていないとCopyが使えない上位魔法だと思えばいい。
@@ -21,7 +19,7 @@ pub enum NovelImg {
 }
 
 impl Novel {
-    pub fn novel_page(&self) -> &'static[&'static str] {
+    pub fn novel_page(&self) -> &'static [&'static str] {
         match self {
             Novel::Novel1 => &text_data::NOVEL1,
             Novel::Novel2 => &text_data::NOVEL2,
@@ -38,17 +36,20 @@ pub fn get_message(novel: Novel, count: usize) -> String {
 }
 
 impl NovelImg {
-    pub fn nimgpath(&self, count:usize) -> Option<&'static str>{
-        match self{
+    pub fn nimgpath(&self, count: usize) -> Option<&'static str> {
+        match self {
             NovelImg::Novel1 => match count {
                 1 => Some("assets/images/ouch.webp"),
                 2 => Some("assets/images/doup.webp"),
+                5 => Some("assets/images/ikuzo.webp"),
                 _ => None,
             },
             NovelImg::Novel2 => match count {
                 0 => Some("assets/images/zenbuyaku.webp"),
+                1 => Some("assets/images/nattsuya-de.webp"),
+                2 => Some("assets/images/hakkou.webp"),
+                3 => Some("assets/images/otosan.webp"),
                 _ => None,
-                
             },
         }
     }
